c03ad44e362a4a02c3940526c6d8ed7895954bde
Сағындық Асылхан Ғабитұлы <asylkhan.sagyndyk@bcchub.kz>
2025-02-10 09:36:07 +0500

    fix: directory

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,12 +1,13 @@
 import { supabase } from "../../../../lib/supabaseClient";
 import { NextRequest } from "next/server";
 
-export const PUT = async (req: NextRequest, { params }: { params: { id: string } }) => {
+export const PUT = async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
     const { status } = await req.json();
     const { error } = await supabase
         .from("todos")
+
         .update({ status })
-        .eq("id", params.id);
+        .eq("id", (await params).id);
 
     if (error) {
         return new Response(JSON.stringify({ error }), { status: 400 });
@@ -16,11 +17,12 @@ export const PUT = async (req: NextRequest, { params }: { params: { id: string }
 }
 
 
-export const POST = async (req: NextRequest, { params }: { params: { id: string } }) => {
+export const POST = async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
   const { error } = await supabase
     .from("todos")
     .update({ status: false })
-    .eq("id", params.id);
+    .eq("id", (await params).id);
+
 
   if (error) {
     return new Response(JSON.stringify({ error }), { status: 400 });
